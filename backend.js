5f7872a30a9355f44d83d5813a8226eef2b5f826
thebest141492 <thebestmusic8086@gmail.com>
2025-06-11 11:22:36 -0600

    prueba

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Configuración para tu SQL Server
+// Configuración para tu SQL Serverr
 const config = {
     user: 'kike',
     password: '04',
